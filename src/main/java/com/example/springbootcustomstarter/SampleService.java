6ff84b805cf2adba8ec357f6c4beeb2736d07bdf
PeronVolodymyr <perwolod@gmail.com>
2022-12-06 21:40:52 +0100

    first commit

---
+++ src/main/java/com/example/springbootcustomstarter/SampleService.java
@@ -0,0 +1,8 @@
+package com.example.springbootcustomstarter;
+
+public class SampleService {
+    public String getSampleMessage() {
+        return "Hello World from 'spring-boot-custom-starter'!";
+    }
+
+}
