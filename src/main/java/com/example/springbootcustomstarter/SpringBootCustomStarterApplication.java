90f29165c588e1060b4cbb5750b1c8f163d4f1ed
PeronVolodymyr <perwolod@gmail.com>
2022-12-06 22:11:20 +0100

    fixes

---
+++ src/main/java/com/example/springbootcustomstarter/SpringBootCustomStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcustomstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomStarterApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCustomStarterApplication.class, args);
+    }
+}
